8d03827ae7815b37c3d60a46497a0d123608af62
Esther <160644164+esthergreene@users.noreply.github.com>
2025-11-28 23:07:59 -0500

    paper.jsx created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import ConferenceInfo from "./pages/ConferenceInfo";
+import PaperPage from "./pages/Paper";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/conference-info" element={<ConferenceInfo />} />
+            <Route path="/papers" element={<PaperPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
