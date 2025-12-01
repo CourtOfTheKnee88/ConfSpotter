d442d9cf637b16b5221b50e8d2e5fd8ffec240db
Seth-Tedder <seth.tedder@maine.edu>
2025-12-01 14:56:44 -0500

    Initial work on sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,11 +1,47 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-function SignUp() {
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log("Signup email:", email, "password:", password);
+  };
+
   return (
     <div>
-      <h1>TEMP</h1>
+      <h2>Signup</h2>
+      <form onSubmit={handleSignup}>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter confirm password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Signup</button>
+      </form>
     </div>
   );
-}
+};
 
-export default SignUp;
+export default Signup;
